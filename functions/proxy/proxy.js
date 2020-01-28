9d470c199e641529699bda48a3e2cdf03f33c60f
Kyle Carter <kcarter@mastercontrol.com>
2020-01-28 16:15:41 -0700

    Getting the proxy working a little better

---
+++ functions/proxy/proxy.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+exports.handler = function(event, context, callback) {
+  let request = JSON.parse(event.body);
+  (async () => {
+     let response = await fetch(request.url, {
+       mode: 'no-cors',
+       cache: 'no-cache',
+       headers: {
+         'Authorization': `Basic ${Buffer.from(`${request.username}:${request.password}`).toString('base64')}`
+       }
+     });
+    let jsonResponse = await response.text();
+    callback(null, {
+      statusCode: 200,
+      body: jsonResponse
+    });
+  })();
+}
+// {
+//   "url":"https://tselastic.mclabs.cloud:9243/_cat/indices",
+//   "method":"GET",
+//   "username":"elastic",
+//   "password":"2q3W@9OU1*41yvmE",
+//   "body": "{\"hello\":\"world\"}"
+// }
